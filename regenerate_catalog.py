536ba2046e6b499952dc8b7fec3da22431e7ca5e
evantayloryates <evantayloryates@gmail.com>
2019-09-30 14:50:57 -0700

    travisCI config now calls correct python script

--- regenerate_catalog.py
+++ regenerate_catalog.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import json
 
 process = subprocess.Popen(["git", "diff", "--name-only", os.environ['TRAVIS_COMMIT_RANGE']], stdout=subprocess.PIPE)
 files = process.communicate()[0].decode("utf-8")
@@ -7,8 +8,11 @@ files = process.communicate()[0].decode("utf-8")
 # Check if changes were made to code.json
 if 'code.json' in files.split():
 	print("REGENERATING CATALOG.JSON")
+	f = open("./catalog.json", "r")
+
 else:
 	print("NO CODE.JSON DIFFS")
+	print(json.loads(f.read())[0]['Software'])
 
 
 
