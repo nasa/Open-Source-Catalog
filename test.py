df1585f77ee876b69e88cc34052d6c88e6feff94
evantayloryates <evantayloryates@gmail.com>
2019-09-30 14:35:26 -0700

    added diff checking to py script

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 import os
+import subprocess
+
 
 print(os.environ['TRAVIS_COMMIT_RANGE'])
+print("\n\n\n")
+process = subprocess.Popen(["git", "diff", "--name-only", os.environ['TRAVIS_COMMIT_RANGE']], stdout=subprocess.PIPE)
+output = process.communicate()[0]
+print(output)
+# CHANGED_FILES = ($(git diff --name-only $TRAVIS_COMMIT_RANGE))
