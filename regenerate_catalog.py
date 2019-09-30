532fd83935d257328331f985c23f13b94b33f023
evantayloryates <evantayloryates@gmail.com>
2019-09-30 15:07:05 -0700

    testing travisCI 're-push'

--- regenerate_catalog.py
+++ regenerate_catalog.py
@@ -2,19 +2,21 @@ import os
 import subprocess
 import json
 
-process = subprocess.Popen(["git", "diff", "--name-only", os.environ['TRAVIS_COMMIT_RANGE']], stdout=subprocess.PIPE)
+# process = subprocess.Popen(["git", "diff", "--name-only", os.environ['TRAVIS_COMMIT_RANGE']], stdout=subprocess.PIPE)
+process = subprocess.Popen(["git", "diff", "--name-only"], stdout=subprocess.PIPE)
 files = process.communicate()[0].decode("utf-8")
 
 # Check if changes were made to code.json
 if 'code.json' in files.split():
 	print("REGENERATING CATALOG.JSON")
-	
-
-else:
-	print("NO CODE.JSON DIFFS")
 	f = open("./catalog.json", "a")
 	f.write("This is a text addition")
 	f.close()
+	process1 = subprocess.Popen(["git", "add", "*"], stdout=subprocess.PIPE)
+	process2 = subprocess.Popen(["git", "commit", "-m", "automatic submit"], stdout=subprocess.PIPE)
+	process3 = subprocess.Popen(["git", "push"], stdout=subprocess.PIPE)
 
-
+else:
+	print("NO CODE.JSON DIFFS")
+	
 
